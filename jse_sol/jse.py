8d47caca2b01bd9467f8a567c6b7679831849284
red <ayosikho@gmail.com>
2025-09-28 05:39:49 +0200

    together with money

--- jse_sol/jse.py
+++ jse_sol/jse.py
@@ -17,6 +17,7 @@ import os
 import pickle
 import threading
 import json
+from jse.gui.watchlist_panel import WatchListPanel
 from scipy import stats
 from statsmodels.tsa.seasonal import seasonal_decompose
 import matplotlib.animation as animation
